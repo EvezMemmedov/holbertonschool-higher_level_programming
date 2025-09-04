81c406cc49591b368c981fa7eb4062643db5008c
Evez Memmedov <evezm254@gmail.com>
2025-09-04 13:25:11 +0000

    Added

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-words = str.split()
-print(words[5]+ " " + words[6] + " " + words[12] + " " + words[0])
+print(str[39:66] + str[106:112] + str[:6])
