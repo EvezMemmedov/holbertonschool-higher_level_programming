41b9bfb918f4c2152cecb45742b1094d4bd00e64
Evez Memmedov <evezm254@gmail.com>
2025-09-07 13:33:49 +0000

    Added

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,10 +6,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
-    operator = int(sys.argv[2])
+    operator = sys.argv[2]
     b = int(sys.argv[3])
     if operator not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        print(f"{a} {operator} {b} = {result}")
+    if operator == "+":
+        result = add(a, b)
+    if operator == "-":
+        result = sub(a, b)
+    if operator == "*":
+        result = div(a, b)
+    if operator == "/":
+        result = mul(a, b)
+    print(f"{a} {operator} {b} = {result}")
