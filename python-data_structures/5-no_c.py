794dd059ffaa2792da8c6f6868162d90129817e9
Evez Memmedov <evezm254@gmail.com>
2025-09-09 13:12:15 +0000

    Added

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for i in my_string:
-        if i != "c" or i != "C":
-            print("{}".format(i), end="")
+        if i != "c" and i != "C":
+            new_string += i
+        return new_string
