18cef7d040de8c803ea2bc435e75564beeeda291
Evez Memmedov <evezm254@gmail.com>
2025-09-09 17:48:39 +0000

    Added

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_ = my_list[0]
     if len(my_list) == 0:
         return None
+    max_ = my_list[0]
     for i in my_list:
         if i > max_:
             max_ = i
