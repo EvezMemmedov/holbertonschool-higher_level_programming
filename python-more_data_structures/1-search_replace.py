51e0975e848051ecf92a1897d3d2f20407c79352
Evez Memmedov <evezm254@gmail.com>
2025-09-11 16:04:22 +0000

    Added

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    new_list[search] = replace
+    new_list = [replace if x == search else x for x in my_list]
     return new_list
