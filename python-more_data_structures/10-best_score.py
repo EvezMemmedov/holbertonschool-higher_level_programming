3a3e5409fdb8f87cd694aa9b410d559b0ff3bd45
Evez Memmedov <evezm254@gmail.com>
2025-09-12 11:53:03 +0000

    Added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_key = None
-    max_value = None
+    max_value = float('-inf')
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
