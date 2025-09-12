ea7bb1418562c40ccbf0a50f8a56b63bf2d604c7
Evez Memmedov <evezm254@gmail.com>
2025-09-12 11:51:44 +0000

    Added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,5 +7,5 @@ def best_score(a_dictionary):
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
-            max_key key
+            max_key = key
     return max_key
