db87f99897658d9afc6a14f16cf416667e0726c6
Evez Memmedov <evezm254@gmail.com>
2025-09-13 20:24:19 +0000

    Added

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    total = 0
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    values = {"I": 1, "V": 5, "X": 10, "L": 50,
+             "C": 100, "D": 500, "M": 1000}
+    for idx in range(len(roman_string)):
+        values = values[roman_string[idx]]
+        if idx + 1 < len(roman_string) and value < values[roman_string[idx + 1]]:
+            total -= values
+        else:
+            total += values
+    return total
