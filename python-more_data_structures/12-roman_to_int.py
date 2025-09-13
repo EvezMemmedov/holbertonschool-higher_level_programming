505368f2b192dc35298d57541780759d1985c2f6
Evez Memmedov <evezm254@gmail.com>
2025-09-13 20:26:31 +0000

    Added

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     values = {"I": 1, "V": 5, "X": 10, "L": 50,
              "C": 100, "D": 500, "M": 1000}
     for idx in range(len(roman_string)):
-        values = values[roman_string[idx]]
+        value = values[roman_string[idx]]
         if idx + 1 < len(roman_string) and value < values[roman_string[idx + 1]]:
             total -= values
         else:
