9fcff4596724f43466ede0c212ac9fe84702cf79
Evez Memmedov <evezm254@gmail.com>
2025-09-17 18:04:45 +0000

    Added

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defien a squere class
+"""
+class Square:
+    """
+    Represents a square.
+    Private instance attribute:
+        __size
+    """
+    def __init__(self, size):
+        """
+        Initialize a new Square.
+        Args:
+            size: initial size of the square (no type/value checks).
+        """
+        self.__size = size
