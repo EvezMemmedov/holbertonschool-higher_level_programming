1a601d66bd2c0040951d509c7eb0c8417ffc76eb
Evez Memmedov <evezm254@gmail.com>
2025-09-25 18:04:02 +0000

    Added

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,9 +6,8 @@ this module retur true or false
 
 def is_same_class(obj, a_class):
     """
-    that return type
-
-    Prototype:def is_same_class(obj, a_class):
+    Return True if obj is an instance of a_class or
+    an instance of a class that inherited from a_class.
     """
     if isinstance(obj, a_class):
         return True
