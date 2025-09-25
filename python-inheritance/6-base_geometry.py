7b3261d69551320185e3b0d7683e0c22842e4b00
Evez Memmedov <evezm254@gmail.com>
2025-09-25 18:58:06 +0000

    Added

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+this module create an empty Square class
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class that defines a square.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
