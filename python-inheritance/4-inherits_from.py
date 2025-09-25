ae6ff379e4f76e4ca6ef5ce12befe81e8b4fa055
Evez Memmedov <evezm254@gmail.com>
2025-09-25 18:18:01 +0000

    Added

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
 
     Prototype:def is_same_class(obj, a_class):
     """
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
