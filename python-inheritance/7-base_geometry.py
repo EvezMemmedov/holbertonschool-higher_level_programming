6d35a8d8c5fdda03effe8f5f53d613cd3abeb88c
Evez Memmedov <evezm254@gmail.com>
2025-09-26 17:05:40 +0000

    Added

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """
-this module create an empty Square class
+This module defines the BaseGeometry class
 """
 
 
 class BaseGeometry:
     """
-    This is an empty class that defines a square.
+    Base class for geometry objects
     """
+
     def area(self):
+        """
+        Raises an exception because area is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validates that value is an integer greater than 0
+
+        Args:
+            name (str): name of the parameter
+            value (int): value to validate
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
