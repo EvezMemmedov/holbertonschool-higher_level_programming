b8a058f8e6b20d28db83e4d2aa92b33d5e6ae064
EvezMemmedov <evezm254@gmail.com>
2025-09-30 22:19:35 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module for reading text files and printing their contents to stdout.
+"""
+
+
 def read_file(filename=""):
+    """Reads a text file and prints its contents to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
+
