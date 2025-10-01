e47c4de29e9da54a02b897b702fb40dc6b8c9e5f
EvezMemmedov <evezm254@gmail.com>
2025-10-01 17:42:10 +0000

    Added

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,7 +13,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 if path.exists(filename):
-    load_from_json_file(filename)
+    items = load_from_json_file(filename)
 else:
     items = []
 items.extend(sys.argv[1:])
