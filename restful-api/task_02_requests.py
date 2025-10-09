d3748b6c6d54854124db3382907df7243fb4100c
EvezMemmedov <evezm254@gmail.com>
2025-10-09 19:11:49 +0000

    added

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print("Status Code:", response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print("post['title']")
+    else:
+        print("Failed to retrieve posts")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code = 200:
+        posts = response.json()
+        data = [{'id': p['id'], 'title': p['title'], 'body': p['body']} for p in posts]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
