f7e8b7ff2be5d55fa2240738d78edc1d7d8c61f9
EvezMemmedov <evezm254@gmail.com>
2025-10-09 19:16:45 +0000

    added

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -22,7 +22,7 @@ def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
 
-    if response.status_code = 200:
+    if response.status_code == 200:
         posts = response.json()
         data = [{'id': p['id'], 'title': p['title'], 'body': p['body']} for p in posts]
 
