6eab04003cf46b5fff3970abcab01c41e980d7f0
Evez Memmedov <evezm254@gmail.com>
2025-10-23 18:24:07 +0000

    Added

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,9 @@
 -- 5-full tables
 -- this scrip print table
-SELECT first_table FROM hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) DEFAULT NULL,
+    c CHAR(1) DEFAULT NULL,
+    created_at DATE DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;=
