a738231fda9c44d5e71967f36c6d622e9fbc215c
Evez Memmedov <evezm254@gmail.com>
2025-10-25 18:41:17 +0000

    Added

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
--- this script lists all shows that have at least one genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- List all shows that have at least one genre linked
+SELECT s.title, g.genre_id FROM tv_shows AS s
+RIGHT JOIN tv_show_genres AS g ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
