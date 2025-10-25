badc42c1532b53167ef3c0955b7a2bdfc909f53a
Evez Memmedov <evezm254@gmail.com>
2025-10-25 17:06:14 +0000

    Added

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 -- Script that creates the MySQL server user user_0d_1
+-- user_0d_1 should have all privileges on your MySQL server
+-- Password: user_0d_1_pwd
+-- If the user already exists, the script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@"localhost" WITH GRANT OPTION;
 FLUSH PRIVILEGES;
