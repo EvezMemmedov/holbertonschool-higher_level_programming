26e5caaf7319ed4e3759da30ed8e584b9e272da8
Evez Memmedov <evezm254@gmail.com>
2025-10-25 16:55:54 +0000

    Added

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- this script show both tables
-SELECT user_0d_1, user_0d_2 FROM FULL JOIN localhost;
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
