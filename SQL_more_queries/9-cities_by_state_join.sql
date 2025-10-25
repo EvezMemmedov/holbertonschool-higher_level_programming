44216c1a745ac512c1ce315005e033e4db9e1223
Evez Memmedov <evezm254@gmail.com>
2025-10-25 18:09:17 +0000

    Added

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- this script lists all cities with their state in hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.states_id = states.id
+ORDER BY cities.id ASC;
