8daefd7fc52614ce8b94700c076f6a5204c74c80
Evez Memmedov <evezm254@gmail.com>
2025-10-25 18:59:29 +0000

    Added

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- List all genres
-SELECT tv_genres.name as g, COUNT(tv_show_genres.show_id) as number_shows
+SELECT tv_genres.name AS genre, 
+       COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
-HAVING number_shows > 0
-ORDER BY number_shows DESC;
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
