1a72ef32dab5faa94645c696dcd791794d1df2d0
Evez Memmedov <evezm254@gmail.com>
2025-10-26 16:09:16 +0000

    Added

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     connct = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=argv[1],
-        passwod=argv[2],
-        db=argv[3],
-        charset="utf-8"
+        user=sys.argv[1],
+        passwod=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
     )
     cur = connct.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDEY BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
