37d948e9cb25548ae2b34c9a86b9988e9753e578
Evez Memmedov <evezm254@gmail.com>
2025-10-26 16:54:32 +0000

    Added

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""
-that takes in arguments and displays all values
-"""
 
+
+"""
+Takes in arguments and displays all values in the states table
+where name matches the given argument (safe from MySQL injection).
+"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        paswrd=argv[2],
-        db=argv[3],
+        user=user,
+        password=password,
+        db=database,
         charset="utf8"
     )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name=%s ORDER BY id ASC", (argv[4]))
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC", (state_name,))
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
-    conn.close()
+    db.close()
