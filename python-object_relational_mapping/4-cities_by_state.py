da94c57f96f48137364cc05574fac00170cd83c7
Evez Memmedov <evezm254@gmail.com>
2025-10-26 17:41:04 +0000

    Added

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("SELECT c.id, c.name, s.name FROM cities AS c\
-        LEFT JOIN states AS s ON c.states_id=s.id\
+        LEFT JOIN states AS s ON c.state_id=s.id\
         ORDER BY c.id ASC")
     rows = cur.fetchall()
 
