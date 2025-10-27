65de74c2e016f82aa759b8dbcb816af3b3ee5096
Evez Memmedov <evezm254@gmail.com>
2025-10-27 17:55:56 +0000

    Added

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,7 +13,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
-    database= sys.argv[3]
+    database = sys.argv[3]
 
     engine = create_engine(
         f'mysql+mysqldb://{user}:{password}@localhost:3306/{database}'
@@ -21,6 +21,10 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
     session = Session()
-    states = session.query(State).filter(func.binary(State.name).like("%a%")).order_by(State.id).all()
+    states = (
+        session.query(State)
+        .filter(func.binary(State.name).like("%a%"))
+        .order_by(State.id).all()
+    )
     for state in states:
         print(f"{state.id}: {state.name}")
