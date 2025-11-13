46399581c08a37117faf9a4519931c9d0233fe70
Evez Memmedov <evezm254@gmail.com>
2025-11-13 14:46:58 +0000

    Added

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,6 +2,6 @@
 function add(a, b) {
   return a + b;
 }
-const first = process.argv[2];
-const second = process.argv[3];
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
 console.log(add(first, second));
